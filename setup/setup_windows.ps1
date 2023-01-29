61a074e79060a57176d5f6532498d0d71eac6d3e
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-29 19:35:40 +0000

    fix: add setup scripts

--- setup/setup_windows.ps1
+++ setup/setup_windows.ps1
@@ -1,1 +1,22 @@
-rustup default stable-msvc
+function Install-Rustup {
+    Write-Host "Please follow the Tauri documentation" -ForegroundColor Green
+    Start-Process "https://next--tauri.netlify.app/next/guides/getting-started/prerequisites/windows"
+
+    Write-Host "Checking if Rustup is already installed" -ForegroundColor Green
+    $rustup = Get-Command rustup -ErrorAction SilentlyContinue
+    if ($rustup) {
+        Write-Host "Rustup is already installed" -ForegroundColor Green
+        Write-Host "Setting Rust Toolchain version" -ForegroundColor Green
+        rustup default stable-msvc
+        
+        return
+    }
+
+    Write-Host "Installing Rustup" -ForegroundColor Green
+    winget install --id Rustlang.Rustup
+
+    Write-Host "Setting Rust Toolchain version" -ForegroundColor Green
+    rustup default stable-msvc
+}
+
+
